80029f3ab9a5fe524634e0c4ce502f79116e96a1
Ben Forbes <benjaminforbes@me.com>
2026-02-25 16:11:21 -0800

    deleting build files

--- build/msg_gripper_ros2/ament_cmake_python/msg_gripper_ros2/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='msg_gripper_ros2',
-    version='1.0.0',
-    packages=find_packages(
-        include=('msg_gripper_ros2', 'msg_gripper_ros2.*')),
-)
